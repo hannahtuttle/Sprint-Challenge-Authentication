8c3a02748d9f3485a0ac878aff74e8f930413601
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-09-20 08:56:20 -0700

    added functionality for auth register and login, working with token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,44 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs')
+const JWT = require('jsonwebtoken')
+const data = require('./auth-router-model.js')
+const Token = require('./authenticationToken.js')
 
 router.post('/register', (req, res) => {
   // implement registration
+  const {username, password} = req.body
+
+  
+  const hash = bcrypt.hashSync(password, 10)
+
+  data.add({username, password: hash})
+  .then(save => {
+    res.status(201).json(save)
+  })
+  .catch(err => {
+    res.status(500).send(err)
+  })
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  const {username, password} = req.body
+
+  data.findBy({username})
+  .first()
+  .then(user => {
+    console.log('user', user)
+    if(user && bcrypt.compareSync(password, user.password)){
+        const token = Token.generateToken(user)
+        res.status(200).json({user: user.username, token: token})
+    }else{
+      res.status(401).json({message: 'invalid credential, you shall not pass'})
+    }
+  })
+  .catch(err => {
+    console.log('err', err)
+    res.status(500).send(err)
+  })
 });
 
 module.exports = router;
